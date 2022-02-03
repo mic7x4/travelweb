32cd6b93ecdb7df85c6b63a0bc56ebcba898ac99
mic7x4 <mic7x4@gmail.com>
2022-02-03 19:18:32 +0200

    scroll reveal

--- js/main.js
+++ js/main.js
@@ -33,3 +33,31 @@ navLinks.forEach(link => link.addEventListener('click', () =>{
     current[0].className = current[0].className.replace(' current', '')
     link.className += ' current'
 } ))
+
+const sr = ScrollReveal({
+    distance: '25px',
+    duration: 2500
+})
+
+sr.reveal(`.image-left, .popular-destinations-data, .plan-trip-data`, {
+    origin: 'left'
+})
+
+sr.reveal('.image-center', {
+    origin: 'bottom'
+})
+sr.reveal(`.image-right, .showcase-data`, {
+    origin: 'right'
+})
+
+sr.reveal('.beach-data-wrapper', {
+    origin: 'bottom',
+    interval:250
+})
+sr.reveal('.newsletter-containerr', {
+    origin: 'top'
+})
+sr.reveal(`.footer-container-inner > div, .footer-separator, .copyright`, {
+    origin: 'bottom',
+    interval:250
+})
