47fd9a860e837e56aca02b779d65d100c6220603
mic7x4 <mic7x4@gmail.com>
2022-02-03 19:07:32 +0200

    the finish up

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const menuToggleIcon = document.getElementById('menu-toggle-icon')
 const navMobile = document.getElementById('nav')
-const headerElement = document.getElementById('header')
-// const menuToggleIcon = document.getElementById('menu-toggle-icon')
+ const headerElement = document.getElementById('header')
+const navLinks = document.querySelectorAll('.nav-mobile .list-link')
+
+console.log(navLinks);
 
 
 const toggleMenu = ()=>{
@@ -14,10 +16,20 @@ menuToggleIcon.addEventListener('click',toggleMenu)
 // Add/Remove header border bottom on Scroll
 const headerScrollEvent = ()=>{
     if(this.scrollY >= 30 ){
-        headerElement.classList.add('active-scroll')
+        headerElement.classList.add('active-scroll');
     }else {
-        headerElement.classList.remove('active-scroll')
+        headerElement.classList.remove('active-scroll');
     }
 }
 
 window.addEventListener('scroll',headerScrollEvent)
+
+// Add selected link styles
+navLinks.forEach(link => link.addEventListener('click', () =>{
+    navMobile.classList.remove('active');
+    headerElement.classList.remove('active')
+
+    let current = document.getElementsByClassName('current')
+    current[0].className = current[0].className.replace(' current', '')
+    link.className += ' current'
+} ))
